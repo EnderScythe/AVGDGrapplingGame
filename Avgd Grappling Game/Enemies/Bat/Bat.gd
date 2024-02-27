2c7cf985bcf780407f27a6559c681a8ece86dad9
sheepapple <sheepapple00@gmail.com>
2024-02-27 10:37:27 -0500

    Added bat

---
+++ Avgd Grappling Game/Enemies/Bat/Bat.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+#State Machine
+var idle = true;
+var aggro = false;
+var attack = false;
+
+#Movement
+var newPos;
+var timeSinceNewPos
+var lastPos
+var rng = RandomNumberGenerator.new();
+var accel = 5;
+#var MAX_SPEED = 2000;
+
+# Called when the node enters the scene tree for the first time.
+
+
+func _ready():
+	newPos = position
+	lastPos = position
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if(velocity.x>10):
+		rotation_degrees = 180
+		scale.y=-1
+	elif(velocity.x<-10):
+		rotation_degrees = 0
+		scale.y=1
+		
+	#print(velocity.x)
+		
+	if position.distance_to(newPos) < 100 or timeSinceNewPos > 5:
+		var rotation = rng.randf_range(0, TAU)
+		newPos = position + Vector2(0, rng.randf_range(150, 450)).rotated(rotation)
+		timeSinceNewPos=0
+	timeSinceNewPos+=delta
+	var accl = position.direction_to(newPos) * accel
+	velocity += accl
+	lastPos = position
+	
+	#$Sprite2D.rotation_degrees+= atan((accl.y-position.y)/(accl.x/position.x))
+	#print(atan((accl.y-position.y)/(accl.x/position.x)))
+	#print(-(velocity.y/10))
+	#rotation = deg_to_rad(-(velocity.y/10))
+	#$Sprite2d.rotation_degrees = deg_to_rad((velocity.y/100))
+	move_and_slide()
