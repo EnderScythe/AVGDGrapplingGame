e37d1234636dff24950a3e07edc7ac392d3348bc
JoshuaMathewww <joshuamathewww@gmail.com>
2024-02-29 10:59:53 -0500

    Menuing merge

--- Avgd Grappling Game/Enemies/Bat/Bat.gd
+++ Avgd Grappling Game/Enemies/Bat/Bat.gd
@@ -11,6 +11,12 @@ var timeSinceNewPos
 var lastPos
 var rng = RandomNumberGenerator.new();
 var accel = 5;
+var vxMax = 727
+var vyMax = 500
+#attack
+var escapeTimer = 0;
+var damage
+var playerPos;
 #var MAX_SPEED = 2000;
 
 # Called when the node enters the scene tree for the first time.
@@ -30,18 +36,43 @@ func _physics_process(delta):
 		
 	#print(velocity.x)
 		
-	if position.distance_to(newPos) < 100 or timeSinceNewPos > 5:
-		var rotation = rng.randf_range(0, TAU)
-		newPos = position + Vector2(0, rng.randf_range(150, 450)).rotated(rotation)
-		timeSinceNewPos=0
+	if idle == true:
+		if position.distance_to(newPos) < 100 or timeSinceNewPos > 5:
+			var newRotation = rng.randf_range(0, TAU)
+			newPos = position + Vector2(0, rng.randf_range(150, 450)).rotated(newRotation)
+			timeSinceNewPos=0
+	elif aggro == true:
+		if(position.distance_to(playerPos) > 2000):
+			escapeTimer+=delta
+			# print(escapeTimer)
+			if escapeTimer >= 5:
+				aggro=false
+				idle=true
+				accel=5 	
+			newPos=playerPos
+		else:
+			escapeTimer=0
+			newPos = playerPos
+		
 	timeSinceNewPos+=delta
 	var accl = position.direction_to(newPos) * accel
 	velocity += accl
 	lastPos = position
-	
+	playerPos = get_parent().get_node("Player").position
 	#$Sprite2D.rotation_degrees+= atan((accl.y-position.y)/(accl.x/position.x))
 	#print(atan((accl.y-position.y)/(accl.x/position.x)))
 	#print(-(velocity.y/10))
 	#rotation = deg_to_rad(-(velocity.y/10))
 	#$Sprite2d.rotation_degrees = deg_to_rad((velocity.y/100))
+	# print(velocity)
+	if velocity.x>vxMax:
+		velocity.x = vxMax
+	elif velocity.y>vyMax:
+		velocity.y=vyMax
 	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body.get_name() == "Player":
+		idle = false
+		aggro = true
+		accel=7.5
