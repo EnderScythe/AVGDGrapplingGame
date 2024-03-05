a6e5301b339c2a01d35e354eee5e77e5ec05a20e
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-03-05 10:41:34 -0500

    Added something, idk

--- Avgd Grappling Game/Shop/shop.gd
+++ Avgd Grappling Game/Shop/shop.gd
@@ -13,7 +13,7 @@ func _process(delta):
 
 func _on_shield_pressed():
 	get_node("ShieldUpgrades/Shield").disabled = true
-	print("Bought a shield!")
+	get_node("PurchaseHistory").text += "+ Shield\n"
 	get_node("ShieldUpgrades/ShieldRecharge").disabled = false
 	get_node("ShieldUpgrades/ShieldStrength").disabled = false
 func _on_shield_mouse_entered():
@@ -22,77 +22,77 @@ func _on_shield_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_shield_recharge_pressed():
-	print("Shield recharge speed increased!")
+	get_node("PurchaseHistory").text += "+ Recharge Speed\n"
 func _on_shield_recharge_mouse_entered():
 	get_node("UpgradeDescription").text = "Upgrade to decrease the time it takes for the shield to start recharging!"
 func _on_shield_recharge_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_shield_strength_pressed():
-	print("Shield strength increased!")
+	get_node("PurchaseHistory").text += "+ Shield strength\n"
 func _on_shield_strength_mouse_entered():
 	get_node("UpgradeDescription").text = "Upgrade to increase the amount of [unit] the shield can take before breaking!"
 func _on_shield_strength_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_grapple_range_pressed():
-	print("Grapple range increased!")
+	get_node("PurchaseHistory").text += "+ Range\n"
 func _on_grapple_range_mouse_entered():
 	get_node("UpgradeDescription").text = "Upgrade to increase the distance which the end of the grapple hook can travel before automatically terminating!"
 func _on_grapple_range_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_grapple_launch_pressed():
-	print("Grapple launch speed increased!")
+	get_node("PurchaseHistory").text += "+ Launch speed\n"
 func _on_grapple_launch_mouse_entered():
 	get_node("UpgradeDescription").text = "Upgrade to increase the speed the end of the grapple hook travels!"
 func _on_grapple_launch_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_grapple_reel_pressed():
-	print("Grapple reel-in speed increased!")
+	get_node("PurchaseHistory").text += "+ Reel-in speed\n"
 func _on_grapple_reel_mouse_entered():
 	get_node("UpgradeDescription").text = "Increases the speed you move towards the end of the grapple hook after it has attached to something!"
 func _on_grapple_reel_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_machine_speed_pressed():
-	print("Increased your move speed!")
+	get_node("PurchaseHistory").text += "+ Move speed\n"
 func _on_machine_speed_mouse_entered():
 	get_node("UpgradeDescription").text = "Increases your base move speed!"
 func _on_machine_speed_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_pick_swing_speed_pressed():
-	print("Increased swing speed!")
+	get_node("PurchaseHistory").text += "+ Swing speed\n"
 func _on_pick_swing_speed_mouse_entered():
 	get_node("UpgradeDescription").text = "Decreases the time between pickaxe swings!"
 func _on_pick_swing_speed_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_booster_rocket_pressed():
-	print("Mounted rockets onto machine!")
+	get_node("PurchaseHistory").text += "+ Rockets\n"
 func _on_booster_rocket_mouse_entered():
 	get_node("UpgradeDescription").text = "Press [key] to activate a rocket propelled booster! Achive insane velocities with this bad boy!"
 func _on_booster_rocket_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_double_jump_pressed():
-	print("Purchased a double jump!")
+	get_node("PurchaseHistory").text += "+ Double jump\n"
 func _on_double_jump_mouse_entered():
 	get_node("UpgradeDescription").text = "Place a pad (able to be placed in the middle of air) to jump again; the pad can be used multiple times!\n\nYou can JUMP ON THE PAD multiple times however you can only PLACE DOWN the pad ONCE."
 func _on_double_jump_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_repulsor_pressed():
-	print("Purchased a repulsor!")
+	get_node("PurchaseHistory").text += "+ Repulsor\n"
 func _on_repulsor_mouse_entered():
 	get_node("UpgradeDescription").text = "Place a bubble which creates an area which redirects enemies elsewhere (useful when trying to mine ores with monsters around)!\n\nThe bubble has a limited duration and placing a bubble consumes one instance."
 func _on_repulsor_mouse_exited():
 	get_node("UpgradeDescription").text = "Hover over a button to expand."
 
 func _on_revive_pressed():
-	print("Purchased a revive!")
+	get_node("PurchaseHistory").text += "+ Revive\n"
 	get_node("Items/Revive").disabled = true
 func _on_revive_mouse_entered():
 	get_node("UpgradeDescription").text = "Place a respawn beacon in a location of your choosing. If you get destroyed, you will be reconstructed at this location after a brief period of time!\n\nONLY ONE REVIVE IS AVAILABLE PER SHOP. REVIVE IS CONSUMED WHEN PLACED."
