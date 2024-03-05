4610e5d4f8e2c23889edcb67d38c8714c13e4f9d
RamG <ramgodbole0@gmail.com>
2024-03-05 16:49:49 -0500

    Added worm to test room

--- Avgd Grappling Game/Enemies/Worm/WormHead.gd
+++ Avgd Grappling Game/Enemies/Worm/WormHead.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 var segment = preload("res://Enemies/Worm/WormSegment.tscn")
 var segments = [self]
+var acceleration = []
 var velocities = []
 #includes head
-var segmentNum = 7
+var segmentNum = 20
+
+
 
 func _ready():
 	for i in range(segmentNum - 1):
@@ -15,14 +18,27 @@ func _ready():
 		velocities.append(Vector2(0, 0))
 
 func _process(delta):
-	var mousePosition = get_viewport().get_mouse_position()
-	velocities[0] = (mousePosition - position).normalized() * 500;
+	#for velocity in velocities:
+		#if position.y <= 1090:
+			#velocity.y = 1
+		#elif position.y >= 5300:
+			#velocity.y = -1
+		#if position.x <= -70:
+			#velocity.x = 1
+		#elif position.x >= 10200:
+			#velocity.x = -1
+		#velocity = velocity.normalized()
+	#var mousePosition = get_viewport().get_mouse_position()
+	var playerPosition = get_parent().get_node("Player").position
+	velocities[0] = (playerPosition - position).normalized() * 500;
+	#velocities[0] += acceleration[0] * delta
 	position += velocities[0] * delta
 	get_node("Sprite2D").set_rotation(velocities[0].angle())
 	for i in range(1, segmentNum):
 		var segmentVector = segments[i - 1].position - segments[i].position
 		var edgeVector = velocities[i - 1].rotated(PI).normalized()
-		velocities[i] = (segmentVector + edgeVector) * 5
+		velocities[i] = (segmentVector + edgeVector) * 2.5
+		#velocities[i] += acceleration[i];
 		segments[i].position += velocities[i] * delta
 		segments[i].get_node("Sprite2D").set_rotation(velocities[i].angle())
 		
