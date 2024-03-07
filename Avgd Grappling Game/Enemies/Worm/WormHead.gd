24423287cb2fdd92545424d9f9cdbacc28dd4bfe
EnderScythe <91348559+EnderScythe@users.noreply.github.com>
2024-03-07 15:58:41 -0500

    Added worm animations

--- Avgd Grappling Game/Enemies/Worm/WormHead.gd
+++ Avgd Grappling Game/Enemies/Worm/WormHead.gd
@@ -5,34 +5,39 @@ var segments = [self]
 var velocities = []
 #includes head
 enum states {idle, aggro}
-var segmentNum = 20
+var segmentNum = 10
 var state
 var startAttack = false
 var accel
 var playerPosition
 var lastPosition
-var SPEED = 2000
+var SPEED = 3000
 var ACCELERATION = 1250
-var OSCILLATION = 10
-var SEGMENT_GAP = 20
-var SPEED_CAP = 3000
-var ANGLE = 0
-#dir is false than angle is between 0 and 45 otherwise its between -45 and 0
-var dir = false
+var OSCILLATION = 50
+var SEGMENT_GAP = 170
+var SPEED_CAP = 2000
 
 func _ready():
+	self.z_index = segmentNum - 1
+	self.rotate(PI/2)
 	state = states.idle
 	for i in range(segmentNum - 1):
 		var wormSegment = segment.instantiate()
 		add_sibling.call_deferred(wormSegment)
 		segments.append(wormSegment)
 		wormSegment.position = position
+		wormSegment.z_index = segmentNum - 1 - i
+		if i % 2 == 0:
+			wormSegment.rotate(3*PI/2)
+		else:
+			wormSegment.rotate(PI/2)
 	for i in range(segmentNum):
 		velocities.append(Vector2(0, 0))
+	
 
 func _process(delta):
-	#var mousePosition = get_viewport().get_mouse_position()
-	lastPosition = playerPosition
+	var mousePosition = get_viewport().get_mouse_position()
+	#lastPosition = playerPosition
 	playerPosition = get_parent().get_node("Player").position
 	if state == states.idle:
 		if playerPosition.distance_to(position) <= 2000:
@@ -45,23 +50,16 @@ func _process(delta):
 			velocities[0] += (playerPosition - position).normalized() * ACCELERATION * delta
 			if(velocities[0].length() >= SPEED_CAP):
 				velocities[0] = velocities[0].normalized() * SPEED_CAP
-			if dir == false:
-				velocities[0] = velocities[0].rotated(delta/1000)
-				ANGLE += 1
-				if ANGLE > 45:
-					dir = true
-			else :
-				velocities[0] = velocities[0].rotated(-delta/1000)
-				ANGLE -= 1
-				if ANGLE < -45:
-					dir = false 
-			velocities[0] = velocities[0].rotated(ANGLE)
 			position += velocities[0] * delta
-			get_node("Sprite2D").set_rotation(velocities[0].angle())
+			$AnimatedSprite2D.play("worm_head_anim")			
+			get_node("AnimatedSprite2D").set_rotation(velocities[0].angle())
 			for i in range(1, segmentNum):
 				var segmentVector = segments[i - 1].position - segments[i].position
 				var edgeVector = velocities[i - 1].rotated(PI).normalized() * OSCILLATION
-				velocities[i] = (segmentVector + edgeVector) * SEGMENT_GAP
+				velocities[i] = (segmentVector + edgeVector) * SPEED_CAP / SEGMENT_GAP
+				if(velocities[i].length() >= SPEED_CAP):
+					velocities[i] = velocities[i].normalized() * SPEED_CAP
 				segments[i].position += velocities[i] * delta
-				segments[i].get_node("Sprite2D").set_rotation(velocities[i].angle())
+				segments[i].get_node("AnimatedSprite2D").play("worm_segment_anim")
+				segments[i].get_node("AnimatedSprite2D").set_rotation(velocities[i].angle())
 			
