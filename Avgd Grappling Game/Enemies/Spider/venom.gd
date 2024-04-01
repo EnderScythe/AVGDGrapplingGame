a349a98252c6f6a6c4f1f86d0a20446cdbfd8c95
nivang <nivangujral@gmail.com>
2024-04-01 10:51:17 -0400

    Venom Throw Changes

--- Avgd Grappling Game/Enemies/Spider/venom.gd
+++ Avgd Grappling Game/Enemies/Spider/venom.gd
@@ -1,22 +1,50 @@
 extends Area2D
 
-const speed = 500
-var velocity = Vector2()
+var speed = 500
+var steer_force = 50.0
+
+var velocity = Vector2.ZERO
+var acceleration = Vector2.ZERO
+var target = null
 
 func _ready():
 	pass
+	
+
+func start(_target):
+	target = _target
 
 func _physics_process(delta):
-	#velocity.x = speed * delta
-	translate(velocity)
-	$AnimatedSprite2D.play("spit")
-	
-	if (velocity.x == 0):
-		queue_free()
+	acceleration += seek()
+	velocity += acceleration * delta
+	#velocity = velocity.clamped(speed)
+	rotation = velocity.angle()
+	position += velocity * delta
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
+func seek():
+	var steer = Vector2.ZERO
+	if target:
+		var desired = (target.position - position).normalized() * speed
+		steer = (desired - velocity).normalized() * steer_force
+	return steer
+
+#func _physics_process(delta): 
+	#
+	##velocity.x = speed * delta
+	#translate(velocity)
+	#$AnimatedSprite2D.play("spit")
+	#
+	#if (velocity.x == 0):
+		#queue_free()
+
+		
+func explode():
 	queue_free()
 
 func _on_body_entered(body):
 	if (body.name == "Player"):
-		queue_free()
+		body.velocity.x = 1000
+		explode()
+
+func _on_lifetime_timeout():
+	explode()
