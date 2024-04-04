5533725633764322852131e210549d43674ae78b
RamG <ramgodbole0@gmail.com>
2024-04-04 14:59:12 -0400

    Added randomized segment functionality for the worm

--- Avgd Grappling Game/Levels/TileMap.gd
+++ Avgd Grappling Game/Levels/TileMap.gd
@@ -3,8 +3,8 @@ extends TileMap
 @onready var player = get_parent().get_node("Player")
 
 func _ready():
-	print(get_used_cells(0))
-
+	#print(get_used_cells(0))
+	pass
 
 func _process(delta):
 	var id = get_cell_source_id(0, Vector2i(player.position) / 144, true)
