ca84b3e84b10b01450cb477f60c41ffc69e88775
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-04-09 11:17:56 -0400

    AAAA
    I wanted to slap this in here before I royally fuck things up

--- Avgd Grappling Game/Buyables/Revive.gd
+++ Avgd Grappling Game/Buyables/Revive.gd
@@ -1,13 +1,6 @@
 extends Item
 
-var boost_value = 500
-const increment = 300
-
-# this is an example script for an item that increases the player's 
-# grapple range by 500px + an additional 300px every time they use the grappling hook
-# this can be used as a template or reference for future item scripts
-
-# Called when the node enters the scene tree for the first time.
+var cur_revives = 0
 func _ready():
 	super._ready()
 	# Will probably change some value to turn on some collision/checks for the shield
@@ -17,17 +10,9 @@ func _ready():
 func _process(delta):
 	pass
 
-func apply_effect():
-	#There's no actual value for this yet
-	player.MAX_LENGTH += boost_value
-
-func deapply_effect():
-	player.MAX_LENGTH -= boost_value
-
-func on_grapple():
-	player.MAX_LENGTH += increment
-	boost_value += increment
-	print(boost_value)
+func _died():
+	print("Would you lose? Nah, I'd win")
+	PlayerVariables.health = PlayerVariables.max_health
 
 func get_upgrade():
 	return "Revive"
--- Avgd Grappling Game/Player/PlayerVariables.gd
+++ Avgd Grappling Game/Player/PlayerVariables.gd
@@ -1,7 +1,9 @@
 extends Node
 
-#const temp_item_res = preload("res://Items/GrappleRangeBoost.tscn") # just to test if items work
+const temp_item_res = preload("res://Buyables/DoubleJump.tscn") # just to test if items work
 
+var health = 100
+var max_health = 100
 var inventory = []
 var swing_rate = 1000 # the number is in milliseconds (so default is 1 swing per second)
 var rocket_vel = 0
@@ -13,6 +15,7 @@ var cost_inc = 0
 
 func _ready():
 	#inventory.append(temp_item_res.instantiate())
+	#get_tree().get_node("Player").inventory.add_item(temp_item_res.instantiate())
 	pass
 
 func _process(delta):
