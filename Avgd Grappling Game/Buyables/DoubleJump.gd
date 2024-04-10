2a41d0c7d4d7904b141da032a703717de7ee1c65
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-04-10 10:48:57 -0400

    Update DoubleJump.gd

--- Avgd Grappling Game/Buyables/DoubleJump.gd
+++ Avgd Grappling Game/Buyables/DoubleJump.gd
@@ -1,7 +1,7 @@
 extends Item
 
-var max_jump = 1
-var cur_jump = 1
+var max_jump = 0
+var cur_jump = 0
 var jump_velocity = -1000.0
 
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +19,12 @@ func _input(event):
 	if event.is_action_pressed("jump") and !player.is_on_floor() and cur_jump > 0:
 		player.velocity.y = jump_velocity
 		cur_jump -= 1
+
+func apply_effect(): # applies initial effects of picking up the item, such as stat changes
+	max_jump += 1
+
+func deapply_effect(): # inverse of apply_effect, completely reverting the changes made
+	max_jump -= 1
 
 func get_upgrade():
 	return "Double Jump"
