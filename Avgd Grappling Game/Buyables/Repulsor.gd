83ec471e66a87c68222fe86f239ee3a81438dee8
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-04-12 01:06:15 -0400

    Finished repulsor

--- Avgd Grappling Game/Buyables/Repulsor.gd
+++ Avgd Grappling Game/Buyables/Repulsor.gd
@@ -1,6 +1,8 @@
 extends Item
 
 var num_repel = 0
+var repulse = preload("res://Buyables/Repulse.tscn").instantiate()
+var throw_vel = 1500
 
 # this is an example script for an item that increases the player's 
 # grapple range by 500px + an additional 300px every time they use the grappling hook
@@ -18,7 +20,14 @@ func _process(delta):
 func _input(event):
 	if event.is_action_pressed("throw") and num_repel > 0:
 		print("throw")
-		player.get_parent().add_child(preload("res://Buyables/Repulse.tscn").instantiate())
+		player.get_parent().add_child(repulse)
+		repulse.position = player.position
+		repulse.translate(Vector2(0,-250))
+		var angle = get_angle_to(get_global_mouse_position())
+		var initial_impulse = Vector2(throw_vel, 0).rotated(angle)
+		var inertia_fac = max(0, repulse.linear_velocity.dot(initial_impulse) / initial_impulse.dot(initial_impulse))
+		repulse.apply_central_impulse(initial_impulse * (1 + inertia_fac))
+		#repulse.translate(Vector2(80,0).rotated(angle))
 		inventory.remove_item(self)
 		num_repel -= 1
 
--- Avgd Grappling Game/Buyables/repulse.gd
+++ Avgd Grappling Game/Buyables/repulse.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +9,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#print(self.linear_velocity)
+	#print(Vector2.ZERO)
+	time += delta
+	if time > .1:
+		if self.linear_velocity == Vector2.ZERO:
+			self.freeze = 1
 
 
 func _on_timer_timeout():
