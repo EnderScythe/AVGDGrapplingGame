b9b9673c3d7a0c94d942f38023f73a16678c7af1
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-04-24 11:11:53 -0400

    Push before testing

--- Avgd Grappling Game/Environmental Hazards/Stalactite/Stalactite.gd
+++ Avgd Grappling Game/Environmental Hazards/Stalactite/Stalactite.gd
@@ -1,8 +1,6 @@
 extends RigidBody2D
 
 var velocity = Vector2.ZERO
-var inside_dmg_area = false
-var time = 0
 var can_dmg = true
 @onready var player = get_parent().get_node("Player")
 
@@ -15,11 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time += delta
-	if inside_dmg_area == true && can_dmg == true:
-		player.take_hit(15, Vector2.ZERO)
-	if abs(rotation_degrees) > 50:
-		can_dmg = false
+	pass
 
 
 func _on_body_entered(body):
@@ -33,8 +27,6 @@ func _on_area_2d_body_entered(body):
 
 
 func _on_dmg_area_body_entered(body):
-	inside_dmg_area = true
-
-
-func _on_dmg_area_body_exited(body):
-	inside_dmg_area = false
+	if can_dmg == true && abs(rotation_degrees) < 50:
+		player.take_hit(15, Vector2.ZERO)
+		can_dmg = false
