e8bde6da259ccbc8f5d3d99affad7d86c3700f0f
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-04-29 01:53:58 -0400

    added crystal sprites to ores

--- Avgd Grappling Game/Levels/TileMap.gd
+++ Avgd Grappling Game/Levels/TileMap.gd
@@ -4,7 +4,7 @@ var time = 0
 var dmgable = -1
 var minable = -1
 const LAVA_ID = [10, 11]
-const ORE_ID = 7
+const ORE_ID = [7, 8]
 @onready var player = get_parent().get_node("Player")
 
 func _ready():
@@ -20,6 +20,6 @@ func _process(delta):
 							   # Fair seemed to be 15 dmg every .75 dmg = 30 dmg every 1.5 secs => 20 dmg per sec => 1 dmg per .05 secs
 			dmgable = time + .05
 	if time > minable:
-		if id == ORE_ID:
+		if id in ORE_ID:
 			PlayerVariables.ores_carried += 4
 			minable = time + PlayerVariables.swing_rate
