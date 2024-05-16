d8b873bae56a48733e95dd6d51d451e66fb3e623
TurtlesAreKin <160047435+TurtlesAreKin@users.noreply.github.com>
2024-05-16 11:19:15 -0400

    Crystal interaction with melee

--- Avgd Grappling Game/Game/Ore.gd
+++ Avgd Grappling Game/Game/Ore.gd
@@ -1,16 +1,24 @@
 extends Area2D
 
 @onready var player = get_node("res://Player/Player.tscn")
+var time = 0
+var white = -1
 var max_hits = 5
 var hits_left = max_hits
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$GPUParticles2D.emitting = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time += delta
+	if time < white:
+		$Sprite2D.modulate = Color(2, 2, 2)
+		$GPUParticles2D.emitting = true
+	else:
+		$Sprite2D.modulate = Color(1, 1, 1)
+		$GPUParticles2D.emitting = false
 
 func _check_amt():
 	if hits_left == 0:
@@ -21,6 +29,7 @@ func _on_area_entered(area):
 	if area.name == "MeleeAtk":
 		hits_left -= 1
 		PlayerVariables.ores_carried += 1
+		white = time + .25
 		_check_amt()
 	
 
