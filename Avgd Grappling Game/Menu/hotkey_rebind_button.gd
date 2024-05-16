13ae183764e0b3903a89abd77491516226fa97e2
JoshuaMathewww <joshuamathewww@gmail.com>
2024-05-16 11:34:05 -0400

    lil push

--- Avgd Grappling Game/Menu/hotkey_rebind_button.gd
+++ Avgd Grappling Game/Menu/hotkey_rebind_button.gd
@@ -33,12 +33,14 @@ func set_text_for_key() -> void:
 	var action_events = InputMap.action_get_events(action_name)
 	var action_event = action_events[0]
 	print(action_event)
+	
 	if action_event is InputEventKey:
 		var action_keycode = OS.get_keycode_string(action_event.physical_keycode)
 		button.text = "%s" % action_keycode
 		print(action_keycode)
-	#elif action_event isb InputEventMouseButton:
-		#print(action_events + "hello")
+	elif action_event is InputEventMouseButton:
+		button.text = "Mouse" + str(action_event.button_index)
+		print(action_event)
 		
 		#pass
 		#if action_event 
@@ -62,18 +64,24 @@ func _on_button_toggled(button_pressed):
 				i.set_process_unhandled_key_input(false)
 		set_text_for_key()
 		
+
 func _unhandled_key_input(event):
 	rebind_action_key(event)
 	button.button_pressed = false
-	
+
+
+
+
 func rebind_action_key(event) -> void:
 	InputMap.action_erase_events(action_name)
 	InputMap.action_add_event(action_name, event)
-	
+	print(action_name)
+	print(event)
 	set_process_unhandled_key_input(false)
 	set_text_for_key()
 	set_action_name()
 		
+
 	
 
 
